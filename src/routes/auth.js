cca3af2ba97313b69623aa3a6dab383825df4279
robert <robertishimwe0@gmail.com>
2022-08-25 11:52:31 +0200

    added google login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import passport from 'passport';
 import googleController from '../controller/googleAuth'
+<<<<<<< HEAD
 import LinkedinController from '../controller/LinkedinAuth';
 import TwitterController from '../controller/twitterAuth';
+=======
+>>>>>>> added google login
 import AuthController from '../controller/Auth'
 import authValidation from '../validations/auth.validation'
 
@@ -16,12 +19,18 @@ const router = Router();
 router.post('/register', registrationValidation, AuthController.registration)
 router.get('/login', loginValidation, AuthController.login)
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+<<<<<<< HEAD
 router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }), googleController.onSuccess)
 router.get('/linkedin', passport.authenticate('linkedin'));
 router.get('/auth/linkedin/callback',passport.authenticate('linkedin', { failureRedirect: '/login'}), LinkedinController.onSuccess);
 router.get('/twitter', passport.authenticate('twitter', { scope: ['profile', 'email'] }));
 router.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/failed' }), TwitterController.onSuccess)
 
+=======
+router.get(
+    '/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/failed' }), googleController.onSuccess)
+>>>>>>> added google login
 // router.post('/send/forgot-password', validateEmail, sendResetPasswordEmail);   template
 
 export default router;  
