5176cd13e30c3b2ea07a9bb959a91c554100dc88
robert <robertishimwe0@gmail.com>
2022-09-01 19:51:11 +0200

    mm

--- src/services/linkedinPassport.js
+++ src/services/linkedinPassport.js
@@ -4,8 +4,8 @@ import { Strategy as LinkedInStrategy } from 'passport-linkedin-oauth2';
 const host = process.env.HOST || 'http://localhost:3000';
 
 passport.use(new LinkedInStrategy({
-  clientID: "77n5sv58iov6tg",
-  clientSecret: "OBJq3bwNK8UcblLg",
+  clientID: process.env.LINKEDIN_CLIENT_ID,
+  clientSecret: process.env.LINKEDIN_CLIENT_SEC,
   callbackURL: `${host}/api/auth/auth/linkedin/callback`,
   scope: ['r_emailaddress', 'r_liteprofile'],
 }, function (accessToken, refreshToken, profile, done) {
--- src/services/twitterPassport.js
+++ src/services/twitterPassport.js
@@ -5,9 +5,9 @@ import { Strategy as TwitterStrategy } from 'passport-twitter';
 const host = process.env.HOST || 'http://localhost:3000';
 
 passport.use(new TwitterStrategy({
-    consumerKey: "uebn7AAHSEM6lrwgeTTTrVbqU",
-    consumerSecret: "haM0LZU9iXhlMcvAMHXbgG3ryWl0P3c391fC1FxdWYujaThJNS",
-    access_token: "AAAAAAAAAAAAAAAAAAAAAKe7gQEAAAAACuv38CyRGC27KpMlBjo%2BCmnFJ1g%3D8NxphQvdu14bkCfanL411FjMT7yXiOQZEoogeelRFKAcxbzXH7",
+    consumerKey: process.env.TWITTER_CLIENT_ID,
+    consumerSecret: process.env.TWITTER_CLIENT_SEC,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
     callbackURL: `${host}/api/auth/auth/twitter/callback`,
 }, function (accessToken, refreshToken, profile, done) {
     return done(null, profile);
